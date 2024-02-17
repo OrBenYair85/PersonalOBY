d6a7efb4215223878e04deebebdd00cd34a0938a
Or Ben Yair <orthe85@gmail.com>
2024-02-17 11:10:19 +0200

    new version

--- script.js
+++ script.js
@@ -16,3 +16,26 @@ document.querySelectorAll('.icons img').forEach(image => {
         tooltip.style.display = (tooltip.style.display === 'block') ? 'none' : 'block';
     });
 });
+
+
+document.getElementById('contact').addEventListener('click', function () {
+    var divs = document.querySelectorAll('.part');
+
+    // Check if the first div is currently visible by checking its display property
+    if (divs[0].style.display !== 'none') {
+        // If the first div is visible, hide the first three divs and show the fourth div
+        divs[0].style.display = 'none';
+        divs[1].style.display = 'none';
+        divs[2].style.display = 'none';
+        divs[3].style.display = 'block';
+    } else {
+        // If the first div is not visible, show the first three divs and hide the fourth div
+        divs[0].style.display = 'flex';
+        divs[1].style.display = 'block';
+        divs[2].style.display = 'block';
+        divs[3].style.display = 'none';
+    }
+
+    // Prevent the default behavior of the button
+    return false;
+});
